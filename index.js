6b0ed993235afbacbe52b5ea3a08f56bbff126d3
Renzo Fernandez <44455679+renzofernandezr@users.noreply.github.com>
2024-03-08 18:20:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const sql = require('mssql');
 const app = express();
 
+// Database configuration
+const dbConfig = {
+    user: 'dev',
+    password: 'LatidoEterno2024',
+    server: 'latido-eterno-server.database.windows.net', 
+    database: 'LatidoEternoDB',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+
+
 // Define a GET route to retrieve a list of vehicles
-app.get('/medallon', (req, res) => {
+app.get('/hola', (req, res) => {
  res.json("hola")
+});
+
+// Endpoint to get a Medallon by UID
+app.get('/medallon/:uid', async (req, res) => {
+    try {
+        const uid = req.params.uid;
+        await sql.connect(dbConfig);
+        const result = await sql.query`SELECT * FROM Maestro_Medallon WHERE UID = ${uid}`;
+
+        if (result.recordset.length > 0) {
+            res.json(result.recordset[0]);
+        } else {
+            res.status(404).send('Medallon not found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred');
+    }
 });
 
 // Start the server on port 3000
